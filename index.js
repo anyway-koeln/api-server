8a2324fd73361313eb7161c47b469952e91900cf
thomasrosen <thomas.rosen@me.com>
2020-12-20 02:21:55 +0100

    changed from express-graphql to apollo-graphql-express

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
-const { buildSchema } = require('graphql')
+const { ApolloServer, gql } = require('apollo-server-express')
 
-const schema = buildSchema(`
+const typeDefs = gql`
   type Query {
     questions(freitext: String): [String]
   }
-`)
+`
 
-const root = {
-  questions: (props) => {
-    console.log(props.freitext)
-    return [props.freitext]
+const resolvers = {
+  Query: {
+    questions: (props) => {
+      console.log(props.freitext)
+      return [props.freitext]
+    }
   }
 }
 
 const app = express()
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
-}))
-app.listen(4000)
-console.log('Running a GraphQL API server at http://localhost:4000/graphql')
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.applyMiddleware({ app, path:'/graphql', cors: true });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+)
+
