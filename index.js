e3fd5aaa22230831217688d7c57622e6a1ac6a61
thomasrosen <thomas.rosen@me.com>
2020-12-20 02:24:03 +0100

    aufräumen

--- index.js
+++ index.js
@@ -21,10 +21,8 @@ const server = new ApolloServer({
   typeDefs,
   resolvers
 })
-
-server.applyMiddleware({ app, path:'/graphql', cors: true });
+.applyMiddleware({ app, path:'/graphql', cors: true })
 
 app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 )
-
