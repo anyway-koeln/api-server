614f1ec988869b2c526fa4b6d9821ed24c956cd2
thomasrosen <thomas.rosen@me.com>
2020-12-20 04:04:44 +0100

    added a basic schema and coresponding resolvers

---
+++ mutations/incident.js
@@ -0,0 +1,5 @@
+module.exports = (parent, args, context, info) => {	
+	return new Promise((resolve,reject)=>{
+		resolve(null)
+	})
+}
--- resolvers.js
+++ resolvers.js
@@ -1,2 +1,27 @@
+const { GraphQLJSON, GraphQLJSONObject } = require('graphql-type-json')
+
+// const {
+// 	GraphQLDate,
+// 	GraphQLTime,
+// 	GraphQLDateTime,
+// } = require('graphql-iso-date')
+
+const query_incident = require('./queries/incident.js')
+const query_questions = require('./queries/questions.js')
+const mutation_incident = require('./mutations/incident.js')
+const mutation_answers = require('./mutations/answers.js')
+
 module.exports = {
+	// JSON: GraphQLJSON,
+	JSONObject: GraphQLJSONObject,
+	// DateTime: GraphQLDateTime,
+
+	Query: {
+		incident: query_incident,
+		questions: query_questions,
+	},
+	Mutation: {
+		incident: mutation_incident,
+		answers: mutation_answers,
+	},
 }
