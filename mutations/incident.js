d103cef29e7dad95c7f7bdf6de2ef6c1268aef4b
thomasrosen <thomas.rosen@me.com>
2021-01-02 18:34:40 +0100

    added sample secret

--- mutations/incident.js
+++ mutations/incident.js
@@ -1,4 +1,6 @@
-module.exports = (parent, args, context, info) => {	
+module.exports = async (parent, args, context, info) => {
+	
+	console.log('test-secret:', await context.getSecret('test') )
 	return new Promise((resolve,reject)=>{
 		resolve(null)
 	})
