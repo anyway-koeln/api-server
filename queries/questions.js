68966cb8f0ddd82aa2c1b9d99a7bba68d1e4d2ab
thomasrosen <thomas.rosen@me.com>
2021-01-02 19:04:25 +0100

    sample questions

--- queries/questions.js
+++ queries/questions.js
@@ -1,5 +1,29 @@
+
+const questions = [
+	{
+		id: 'q1',
+		question: 'Wie viele Täter?',
+		description: 'Beschreibung',
+		in_one_word: 'Täter',
+		icon: '',
+		answer_type: 'number',
+		answer_options: {},
+		answer_value_suggestion: {},
+	},
+	{
+		id: 'q2',
+		question: 'Wo war es?',
+		description: 'Beschreibung',
+		in_one_word: 'Ort',
+		icon: '',
+		answer_type: 'address',
+		answer_options: {},
+		answer_value_suggestion: {},
+	},
+]
+
 module.exports = (parent, args, context, info) => {	
 	return new Promise((resolve,reject)=>{
-		resolve(null)
+		resolve(questions)
 	})
 }
