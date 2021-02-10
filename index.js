029d561f8238966378dc91697794c8d3be9e068a
thomasrosen <thomas.rosen@me.com>
2021-02-10 08:47:25 +0100

    link to the graphql api endpoint

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ new ApolloServer({
 app.get('/', (req, res) => {
     res.send(`
         <title>anyway API</title>
-        View the API at ./graphql<br>
         Call the self update script at ./self_update<br>
+        View the API at <a href="./graphql">./graphql</a><br>
         <hr>
         Repo: <a href="https://github.com/anyway-koeln/api-server">https://github.com/anyway-koeln/api-server</a>
     `)
