3b022bde6fbcab4b709e7399aca0fd0045b289f2
thomasrosen <thomas.rosen@me.com>
2021-02-10 10:09:16 +0100

    added loading file contents
    it's now ready for the DB and to index it

--- src/git/update_indexes.js
+++ src/git/update_indexes.js
@@ -1,12 +1,46 @@
 const { Octokit } = require('@octokit/core')
 const { getSecret } = require('../secretManager.js')
 const { load_data_tree } = require('./functions.js')
+const async = require('async')
 
-function annotate(file){
+function annotate_file(file, callback) {
     if (file.path.endsWith('.json')) {
         file.content_json = JSON.parse(file.content_raw) || null
     }
-    return file
+    callback(file)
+}
+
+async function load_content(owner, repo, file_sha) {
+    const octokit = new Octokit({ auth: await getSecret('token') })
+
+    const response = await octokit.request('GET /repos/{owner}/{repo}/git/blobs/{file_sha}', {
+        owner,
+        repo,
+        file_sha,
+    })
+    return response
+}
+
+function add_to_db(owner, repo, file, callback) {
+    if (true) { // TODO: if file.path does not exist with file.sha 
+        load_content(owner, repo, file.sha)
+        .then(response => {
+            annotate_file({
+                path: file.path,
+                sha: file.sha,
+                mode: file.mode,
+                type: file.type,
+                size: file.size,
+                content_raw: Buffer.from(response.data.content, 'base64').toString('utf-8'),
+            }, file => {
+                console.log('file', file)
+                callback()
+            })
+        })
+        .catch(callback)
+    } else {
+        callback()
+    }
 }
 
 function self_update () {
@@ -14,38 +48,19 @@ function self_update () {
         const owner = await getSecret('owner')
         const repo = await getSecret('incident_repo')
 
-        const octokit = new Octokit({ auth: await getSecret('token') })
-
-        load_data_tree({
-            owner,
-            repo,
-        })
+        load_data_tree({ owner, repo })
         .then(tree => {
-            Promise.all(
-                tree
-                .filter(file => true)
-                .map(async file => {
-                    const response = await octokit.request('GET /repos/{owner}/{repo}/git/blobs/{file_sha}', {
-                        owner,
-                        repo,
-                        file_sha: file.sha,
-                    })
+            async.each([tree[0]], (file, callback) => {
+                add_to_db(owner, repo, file, callback)
+            }, error => {
+                if (error) {
+                    console.log('A file failed to process')
+                } else {
+                    console.log('All files have been processed successfully')
+                }
 
-                    return annotate({
-                        path: file.path,
-                        sha: file.sha,
-                        mode: file.mode,
-                        type: file.type,
-                        size: file.size,
-                        content_raw: Buffer.from(response.data.content, 'base64').toString('utf-8'),
-                    })
-                })
-            )
-            .then(tree => {
-                console.log(tree)
+                resolve(true)
             })
-
-            resolve(true)
         })
         .catch(reject)
     })
