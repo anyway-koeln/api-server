0659fe35e489a1403cf1097b38bde76782e3ffbb
thomasrosen <thomas.rosen@me.com>
2021-02-10 09:11:55 +0100

    find out if pull request was merged or opend and trigger an indexes update

--- index.js
+++ index.js
@@ -41,14 +41,26 @@ app.get('/self_update', (req, res) => {
 })
 
 app.post('/self_update', (req, res) => {
-    console.log('POST')
-    console.log(req.body)
-    
-    res.send('-')
+    const payload = req.body
+    console.log(payload)
 
-    // update_indexes()
-    // .then(() => res.send('updated'))
-    // .catch(err => res.send('error'))
+    if (
+        payload.action === 'closed'
+        && payload.pull_request.state === 'closed'
+        && payload.pull_request.merged === true
+    ) {
+        // Update Indexes
+        update_indexes()
+        .then(() => res.send('updated'))
+        .catch(error => res.send('error'))
+    } else if (
+        payload.action === 'opened'
+        && payload.pull_request.state === 'open'
+        && payload.pull_request.merged === false
+    ) {
+        res.send('opened')
+        // TODO send info about new data to review
+    }
 })
 
 const port = 4000
