696aa322d1ddf21797d39cf6e6d3ed0a8ee3c382
thomasrosen <thomas.rosen@me.com>
2021-02-12 01:31:36 +0100

    bugfix close the db-connection after loading finished

--- src/queries/incident.js
+++ src/queries/incident.js
@@ -4,22 +4,21 @@ const { getSecret } = require('../secretManager.js')
 async function load_incident(incidentID) {
     return new Promise(async (resolve, reject) => {
         const client = new MongoClient(await getSecret('mongodb_url'), { useUnifiedTopology: true })
-        try {
-            await client.connect()
+        
+        client.connect()
+        .then(() => {
             const database = client.db('cache')
             const collection = database.collection('incidents')
 
             collection.findOne({ basename: incidentID }, {})
-            .then((incident) => {
+            .then(async (incident) => {
                 console.log('incident', incident)
+                await client.close()
                 resolve(incident)
             })
             .catch(reject)
-        } catch (error) {
-            reject(error)
-        // } finally {
-        //     await client.close()
-        }
+        })
+        .catch(reject)
     })
 }
 
